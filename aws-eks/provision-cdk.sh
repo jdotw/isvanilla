0c1fba498a0c8662895fa6757b27ef6efe0cc1ef
James Wilson <jw@jameswilson.io>
2022-01-18 20:40:37 +1100

    consume new jk8s module

--- aws-eks/provision-cdk.sh
+++ aws-eks/provision-cdk.sh
@@ -1,11 +1,10 @@
 #!/bin/zsh
 
-AWS_PROFILE=test-cluster
+AWS_PROFILE=isvanilla
 CDK_DEFAULT_ACCOUNT=132602212048
 CDK_DEFAULT_REGION=ap-southeast-2
-STACK_NAME=isvanilla
 
 cd cdk
 aws iam create-service-linked-role --aws-service-name es.amazonaws.com
-STACK_NAME=$STACK_NAME cdk deploy --all
+cdk deploy --all
 
