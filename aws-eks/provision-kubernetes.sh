b20c1fb3ed35fb2c00af775c0ae956ef935cd1b6
James Wilson <jw@jameswilson.io>
2022-01-21 09:26:47 +1100

    - adding crossplaneServiceAccountName and service account - added crossplane CR for provisioning RDS database

--- aws-eks/provision-kubernetes.sh
+++ aws-eks/provision-kubernetes.sh
@@ -2,43 +2,43 @@
 
 export AWS_PROFILE=isvanilla
 
-CLUSTER_NAME=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='ClusterName'].OutputValue" --output text)
+CLUSTER_NAME=$(aws cloudformation describe-stacks --stack-name EKSStack --query "Stacks[0].Outputs[?OutputKey=='ClusterName'].OutputValue" --output text)
 echo "CLUSTER_NAME: $CLUSTER_NAME"
 
-ARGOCD_SECRETS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='ArgoCDSecretsPolicyARN'].OutputValue" --output text)
+ARGOCD_SECRETS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name SecretsStack --query "Stacks[0].Outputs[?OutputKey=='ArgoCDSecretsPolicyARN'].OutputValue" --output text)
 echo "ARGOCD_SECRETS_ROLE_ARN: $ARGOCD_SECRETS_ROLE_ARN"
 
-TELEMETRY_SECRETS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='TelemetrySecretsPolicyARN'].OutputValue" --output text)
+TELEMETRY_SECRETS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name SecretsStack --query "Stacks[0].Outputs[?OutputKey=='TelemetrySecretsPolicyARN'].OutputValue" --output text)
 echo "TELEMETRY_SECRETS_ROLE_ARN: $TELEMETRY_SECRETS_ROLE_ARN"
-JAEGER_SECRETS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='JaegerSecretsPolicyARN'].OutputValue" --output text)
+JAEGER_SECRETS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name SecretsStack --query "Stacks[0].Outputs[?OutputKey=='JaegerSecretsPolicyARN'].OutputValue" --output text)
 echo "JAEGER_SECRETS_ROLE_ARN: $JAEGER_SECRETS_ROLE_ARN"
-APP_SECRETS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='AppSecretsPolicyARN'].OutputValue" --output text)
+APP_SECRETS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name SecretsStack --query "Stacks[0].Outputs[?OutputKey=='AppSecretsPolicyARN'].OutputValue" --output text)
 echo "APP_SECRETS_ROLE_ARN: $APP_SECRETS_ROLE_ARN"
-DNS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='ClusterDNSPolicyARN'].OutputValue" --output text)
+CROSSPLANE_SECRETS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name SecretsStack --query "Stacks[0].Outputs[?OutputKey=='CrossplaneSecretsPolicyARN'].OutputValue" --output text)
+echo "CROSSPLANE_SECRETS_ROLE_ARN: $CROSSPLANE_SECRETS_ROLE_ARN"
+DNS_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name DNSStack --query "Stacks[0].Outputs[?OutputKey=='ClusterDNSPolicyARN'].OutputValue" --output text)
 echo "DNS_ROLE_ARN: $DNS_ROLE_ARN"
 
 USER_ARN=$(aws iam get-user --query 'User.Arn' --output text)
 echo "USER_ARN: $USER_ARN"
 
-FQDN=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='FQDN'].OutputValue" --output text)
+FQDN=$(aws cloudformation describe-stacks --stack-name DNSStack --query "Stacks[0].Outputs[?OutputKey=='FQDN'].OutputValue" --output text)
 echo "FQDN: $FQDN"
-ZONE_ID=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='ZoneID'].OutputValue" --output text)
+ZONE_ID=$(aws cloudformation describe-stacks --stack-name DNSStack --query "Stacks[0].Outputs[?OutputKey=='ZoneID'].OutputValue" --output text)
 echo "ZONE_ID: $ZONE_ID"
 
-ES_DOMAIN=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='OpenSearchDomain'].OutputValue" --output text)
+ES_DOMAIN=$(aws cloudformation describe-stacks --stack-name OpenSearchStack --query "Stacks[0].Outputs[?OutputKey=='OpenSearchDomain'].OutputValue" --output text)
 echo "ES_DOMAIN: $ES_DOMAIN"
-ES_SECRET=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='MasterUserSecretName'].OutputValue" --output text | sed 's/.*:secret:\([^:]*\):.*/\1/' | sed 's/-[^-]*$//')
+ES_SECRET=$(aws cloudformation describe-stacks --stack-name OpenSearchStack --query "Stacks[0].Outputs[?OutputKey=='MasterUserSecretName'].OutputValue" --output text | sed 's/.*:secret:\([^:]*\):.*/\1/' | sed 's/-[^-]*$//')
 echo "ES_SECRET: $ES_SECRET"
 
-RDS_SECRET=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='RDSSecretName'].OutputValue" --output text)
+RDS_SECRET=$(aws cloudformation describe-stacks --stack-name RDSStack --query "Stacks[0].Outputs[?OutputKey=='RDSSecretName'].OutputValue" --output text)
 echo "RDS_SECRET: $RDS_SECRET"
 
-RDS_HOST=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?OutputKey=='RDSHost'].OutputValue" --output text)
+RDS_HOST=$(aws cloudformation describe-stacks --stack-name RDSStack --query "Stacks[0].Outputs[?OutputKey=='RDSHost'].OutputValue" --output text)
 echo "RDS_HOST: $RDS_HOST"
 
-CLUSTER_STACK_NAME=$(aws cloudformation describe-stacks --stack-name Vanilla --query "Stacks[0].Outputs[?starts_with(OutputKey, 'ClusterStackName')].OutputValue" --output text)
-echo "CLUSTER_STACK_NAME: ${CLUSTER_STACK_NAME}"
-KUBECTL_CONFIG=$(AWS_PROFILE=isvanilla aws cloudformation describe-stacks --stack-name ${CLUSTER_STACK_NAME} --query "Stacks[0].Outputs[?starts_with(OutputKey, 'ClusterConfigCommand')].OutputValue" --output text)
+KUBECTL_CONFIG=$(AWS_PROFILE=isvanilla aws cloudformation describe-stacks --stack-name EKSStack --query "Stacks[0].Outputs[?starts_with(OutputKey, 'ClusterConfigCommand')].OutputValue" --output text)
 echo "KUBECTL_CONFIG: ${KUBECTL_CONFIG}"
 /bin/sh -c "${KUBECTL_CONFIG}"
 
@@ -109,12 +109,28 @@ eksctl create iamserviceaccount --cluster=$CLUSTER_NAME \
   --override-existing-serviceaccounts \
   --approve
 
+# crossplane external secrets
+eksctl create iamserviceaccount --cluster=$CLUSTER_NAME \
+  --name=external-secrets \
+  --namespace=crossplane \
+  --attach-policy-arn=$CROSSPLANE_SECRETS_ROLE_ARN \
+  --override-existing-serviceaccounts \
+  --approve
 
 # Add Helm Chart Repo
 helm repo add jk8s https://jdotw.github.io/jk8s
 helm repo update
 
 # Install jk8s bootstrap Helm Chart
+FQDN=$FQDN \
+  ZONE_ID=$ZONE_ID \
+  ES_DOMAIN=$ES_DOMAIN \
+  ES_SECRET=$ES_SECRET \
+  RDS_SECRET=$RDS_SECRET \
+  RDS_HOST=$RDS_HOST \
+  envsubst < values.yaml | helm upgrade jk8s jk8s/bootstrap --install -n jk8s --create-namespace -f -
+
+# It always fails the first time so... run it agsin 
 FQDN=$FQDN \
   ZONE_ID=$ZONE_ID \
   ES_DOMAIN=$ES_DOMAIN \
