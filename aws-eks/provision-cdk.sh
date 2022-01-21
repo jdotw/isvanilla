a5577d44de26ef6923757239e24e86be74af2660
James Wilson <jw@jameswilson.io>
2022-01-21 15:30:50 +1100

    deploy without confirm

--- aws-eks/provision-cdk.sh
+++ aws-eks/provision-cdk.sh
@@ -6,5 +6,5 @@ aws --profile isvanilla iam create-service-linked-role --aws-service-name es.ama
 AWS_PROFILE=isvanilla \
   CDK_DEFAULT_ACCOUNT=132602212048 \
   CDK_DEFAULT_REGION=ap-southeast-2 \
-  cdk deploy --all
+  cdk deploy --all --require-approval never
 
